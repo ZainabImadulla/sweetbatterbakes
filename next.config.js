04054d0ff78ff0b69ae27827a4dfe2d6560e96f2
ZainabImadulla <113566112+ZainabImadulla@users.noreply.github.com>
2023-01-29 18:04:32 +0530

    im begging

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ module.exports = {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
-        pathname: '/my-account-name/**',
+        pathname: '/dnldvhhyw/**',
       },
     ],
   },
