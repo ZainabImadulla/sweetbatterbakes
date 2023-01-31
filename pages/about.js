5cfae0a28f185cd207c77e0535f1094ecc366354
ZainabImadulla <113566112+ZainabImadulla@users.noreply.github.com>
2023-01-31 18:06:45 +0530

    pleaseeee

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,5 @@
 import {sbb_gif} from "public/images/SBB.gif"
 import Image from 'next/image'
-import { Dropdown } from "@nextui-org/react";
 
 export default function about() {
     
@@ -13,20 +12,6 @@ export default function about() {
             <div className = "our_story w-4/6 sm:mt-12 sm:mr-12 text-center font-serif sm:ml-12">
                 <h1 className = "header mt-10 text-2xl pb-6 md:text-3xl "> Our Story</h1>
                 <p className = "blurb leading-loose pb-12"> Sweet Batter Bakes was founded in 2021 by Abir Khan. She started this business right out her house in Connecticut. She loves to experiment with different flavors and treats which you can check out her instagram @sweetbatterbakes </p>
-            </div>
-            <div>
-            <Dropdown>
-                <Dropdown.Button flat>Trigger</Dropdown.Button>
-                <Dropdown.Menu aria-label="Static Actions">
-                    <Dropdown.Item key="new">New file</Dropdown.Item>
-                    <Dropdown.Item key="copy">Copy link</Dropdown.Item>
-                    <Dropdown.Item key="edit">Edit file</Dropdown.Item>
-                    <Dropdown.Item key="delete" color="error">
-                    Delete file
-                    </Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-            
             </div>
            
         
--- pages/menu.js
+++ pages/menu.js
@@ -13,7 +13,8 @@ import {Cake} from "public/images/i10.png"
 import {Banana} from "public/images/banana_bread.png"
 import {Turnover} from "public/images/turnovers.png"
 import {Madeline} from "public/images/madeline.png"
-
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 export default function menu(){
     return(
@@ -29,6 +30,11 @@ export default function menu(){
                         <div class="card-body pt-2">
                             <h5 class="card-title">Stuffed Cookies</h5>
                             <p class="card-text">Dozen/Half Dozen</p>
+                            <DropdownButton className = "dropdown-button" id="dropdown-basic-button" title="Flavors">
+                                <Dropdown.Item as="button">Brownie Batter Stuffed</Dropdown.Item>
+                                <Dropdown.Item as="button">Red Velvet</Dropdown.Item>
+                                <Dropdown.Item as="button">Hazelnut Churro</Dropdown.Item>
+                            </DropdownButton>
                         </div>
                 </div>
 
