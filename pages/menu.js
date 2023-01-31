bba6ef37e879c8de262bb0fd14a206019d39c543
ZainabImadulla <113566112+ZainabImadulla@users.noreply.github.com>
2023-01-31 18:01:54 +0530

    please please please

--- pages/menu.js
+++ pages/menu.js
@@ -13,12 +13,11 @@ import {Cake} from "public/images/i10.png"
 import {Banana} from "public/images/banana_bread.png"
 import {Turnover} from "public/images/turnovers.png"
 import {Madeline} from "public/images/madeline.png"
-import { Dropdown } from "@nextui-org/react";
+
 
 export default function menu(){
     return(
         <div className = 'products pt-12 font-serif text-center bg-blush h-full'>
-
             <h1 className = "head mt-10 text-2xl pb-6 md:text-3xl  ">
                 Flavor Preview
             </h1>  
@@ -30,20 +29,6 @@ export default function menu(){
                         <div class="card-body pt-2">
                             <h5 class="card-title">Stuffed Cookies</h5>
                             <p class="card-text">Dozen/Half Dozen</p>
-                            <div>
-                                <Dropdown>
-                                    <Dropdown.Button flat>Trigger</Dropdown.Button>
-                                    <Dropdown.Menu aria-label="Static Actions">
-                                        <Dropdown.Item key="new">New file</Dropdown.Item>
-                                        <Dropdown.Item key="copy">Copy link</Dropdown.Item>
-                                        <Dropdown.Item key="edit">Edit file</Dropdown.Item>
-                                        <Dropdown.Item key="delete" color="error">
-                                        Delete file
-                                        </Dropdown.Item>
-                                    </Dropdown.Menu>
-                                </Dropdown>
-                            </div>
-
                         </div>
                 </div>
 
